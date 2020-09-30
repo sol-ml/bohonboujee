72841ac8ec3703be5b28820db525030675fe58c3
Sol Gladinier <sol.gladinier25@gmail.com>
2020-09-30 12:22:15 -0300

    wip

---
+++ src/components/Subtittle.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<style>
+	h2 {
+		font-family: Open Sans;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 37px;
+		width: 290px;
+		height: 44px;
+	}
+</style>
+
+<h2>
+	{text}
+</h2>
