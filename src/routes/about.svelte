14076e849787d1938c2b42e04362e11baab581ce
Sol Leal <solleal13@gmail.com>
2020-10-02 17:29:33 -0300

    add hero component

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,20 @@
+<script>
+	/* 	import ImageWithGoldenBorder from "../components/ImageWithGoldenBorder.svelte"; */
+
+	import Hero from "../components/Hero.svelte";
+</script>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
+
+<!-- <ImageWithGoldenBorder imgSrc="/images/hero/about_desktop.png" /> -->
+
+<Hero
+	imgHeroDesktop="/images/hero/about_desktop.png"
+	imgHeroMobile="/images/hero/about_mobile.png"
+/>
 
 <p>This is the 'about' page. There's not much here.</p>
