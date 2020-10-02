3c81d0b380d9a2d844a44241ddae24900604654d
Sol Leal <solleal13@gmail.com>
2020-10-02 17:25:47 -0300

    wip

---
+++ src/components/Input.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<style>
+	input {
+		width: 38.51%;
+		font-weight: normal;
+		font-family: "Open Sans";
+		background: #ffffff;
+		border: 1px solid #bda37b;
+	}
+</style>
+
+<input placeholder="enter your email " />
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,13 @@
+<script>
+	import ImageWithGoldenBorder from "../components/ImageWithGoldenBorder.svelte";
+</script>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
+
+<ImageWithGoldenBorder imgSrc="/images/hero/about_desktop.png" />
 
 <p>This is the 'about' page. There's not much here.</p>
