927af1994e51a818d98db9d563594b5f0b1d7e48
Sol Gladinier <sol.gladinier25@gmail.com>
2020-10-05 12:42:36 -0300

    add title with background

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,13 @@
+<script>
+	import TitleBackground from "../components/TitleBackground.svelte";
+</script>
+
 <style>
 	h1,
 	figure,
 	p {
 		text-align: center;
 		margin: 0 auto;
-	}
-
-	h1 {
-		font-family: "Open Sans";
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
 	}
 
 	img {
@@ -33,25 +25,7 @@
 			font-size: 4em;
 		}
 	}
-
-	h2 {
-		font-family: "Prompt";
-	}
 </style>
 
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-<h2>HOME SHOP</h2>
-
-<figure>
-	<img alt="Success Kid" src="successkid.jpg" />
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p>
-	<strong>Try editing this file (src/routes/index.svelte) to test live
-		reloading.</strong>
-</p>
+<TitleBackground>Shop Now</TitleBackground>
+<TitleBackground>Shop Accesories</TitleBackground>
