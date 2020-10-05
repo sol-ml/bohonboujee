57989bb2e9181c0d65769b67a5f553ee03bc9124
Sol Gladinier <sol.gladinier25@gmail.com>
2020-10-05 13:53:15 -0300

    fix code

--- src/components/Subtittle.svelte
+++ src/components/Subtittle.svelte
@@ -1,19 +1,17 @@
 <style>
 	h2 {
 		font-family: "Open Sans";
-		font-style: normal;
 		font-weight: bold;
 		font-size: 37px;
 		text-align: center;
-		margin: auto;
 		letter-spacing: 0.03em;
 	}
 
 	.line {
 		margin-top: 40px;
-		border: 1px solid #bda37b;
+		border: none;
+		border-top: 1px solid #bda37b;
 		width: 187px;
-		border-bottom: none;
 	}
 </style>
 
