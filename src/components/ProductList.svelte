96590c6e0c1be5029b420fb22b53d13ec1378804
Sol Leal <solleal13@gmail.com>
2020-11-02 13:35:10 -0300

    product list

---
+++ src/components/ProductList.svelte
@@ -0,0 +1,42 @@
+<script>
+	import ImageWithGoldenBorder from "./ImageWithGoldenBorder.svelte";
+	export let imgSrc;
+	export let text;
+	export let price;
+	export let id;
+</script>
+
+<style>
+	div {
+		font-family: Prompt;
+		font-size: 24px;
+		line-height: 36px;
+		letter-spacing: 0.03em;
+	}
+
+	.price {
+		color: #bda37b;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	@media (max-width: 767.88px) {
+		.text {
+			margin-top: 10px;
+		}
+	}
+
+	@media (min-width: 768px) {
+		.text {
+			margin-top: 20px;
+		}
+	}
+</style>
+
+<a target="_blank" href="https://www.google.com/?hl=es{id}">
+	<ImageWithGoldenBorder imgSrc="{imgSrc}" />
+	<div class="text">{text}</div>
+	<div class="price">{price}</div>
+</a>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,8 +1,61 @@
 <script>
-  import ImageWithGoldenBorder from "../components/ImageWithGoldenBorder.svelte";
+	import ImageWithGoldenBorder from "../components/ImageWithGoldenBorder.svelte";
 	import Hero from "../components/Hero.svelte";
 	import Input from "../components/Input.svelte";
 	import Textarea from "../components/Textarea.svelte";
+	import List from "../components/List.svelte";
+
+	let products = [
+		{
+			imgSrc: "/images/products/necklaces.png",
+			text: "Minimal Double-Strand Hoop Pendant Necklace",
+			price: "$35",
+			id: "?hl=es",
+		},
+
+		{
+			imgSrc: "/images/products/handbag.png",
+			text: "Minimal Double-Strand Hoop Pendant Necklace",
+			price: "$35",
+			id: "?hl=es",
+		},
+
+		{
+			imgSrc: "/images/products/earrings_1.png",
+			text: "Minimal Double-Strand Hoop Pendant Necklace",
+			price: "$35",
+		},
+
+		{
+			imgSrc: "/images/products/earrings_2.png",
+			text: "Minimal Double-Strand Hoop Pendant Necklace",
+			price: "$35",
+		},
+
+		{
+			imgSrc: "/images/products/earrings_3.png",
+			text: "Minimal Double-Strand Hoop Pendant Necklace",
+			price: "$35",
+		},
+
+		{
+			imgSrc: "/images/products/bracelet_1.png",
+			text: "Minimal Double-Strand Hoop Pendant Necklace",
+			price: "$35",
+		},
+
+		{
+			imgSrc: "/images/products/earrings_4.png",
+			text: "Minimal Double-Strand Hoop Pendant Necklace",
+			price: "$35",
+		},
+
+		{
+			imgSrc: "/images/products/bracelet_2.png",
+			text: "Minimal Double-Strand Hoop Pendant Necklace",
+			price: "$35",
+		},
+	];
 
 	let value;
 	let value1 = "hola soy pepito";
@@ -26,3 +79,5 @@
 	imgHeroDesktop="/images/hero/about_desktop.png"
 	imgHeroMobile="/images/hero/about_mobile.png"
 />
+
+<List products="{products}" />
